8febb8f015a44d6929c5029f5e3b47e69fabbc51
John Selfors <selforsjd@msoe.edu>
2017-12-19 01:11:39 +0000

    fixed install script

--- Pi3Installs.sh
+++ Pi3Installs.sh
@@ -5,20 +5,20 @@ sudo apt-get install screen -y
 
 
 # install CMUSphix
-sudo wget https://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz/download -O sphinxbase.tar.gz -y
-sudo wget https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz/download -O pocketsphinx.tar.gz -y
+sudo wget -v -O sphinxbase.tar.gz https://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz/download
+sudo wget -v -O pocketsphinx.tar.gz https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz/download
 
-tar -xzvf sphinxbase.tar.gz -y
-tar -xzvf pocketsphinx.tar.gz -y
+tar -xzvf sphinxbase.tar.gz
+tar -xzvf pocketsphinx.tar.gz
 
-sudo apt-get install bison libasound2-dev swig -y
+sudo apt-get install bison libasound2-dev swig python-dev -y
 
 cd sphinxbase-5prealpha
 ./configure --enable-fixed
 make
-sudo make install -y
+sudo make install
 
 cd ../pocketsphinx-5prealpha
 ./configure
 make
-sudo make install -y
+sudo make install
