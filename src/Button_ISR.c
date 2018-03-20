291bd675908a686707bde650a73ba8c22b87032d
Reid Rumack <rumackr@msoe.edu>
2018-03-20 22:15:14 +0000

    Button_ISR files updated from Johns test files

--- src/Button_ISR.c
+++ src/Button_ISR.c
@@ -12,11 +12,11 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#define BCM2708_PERI_BASE		0X20000000
-#define GPIO_BASE			(BCM2708_PERI_BASE + 0X00200000)
+#define BCM2708_PERI_BASE        0X20000000
+#define GPIO_BASE            (BCM2708_PERI_BASE + 0X00200000)
 
-#define PAGE_SIZE			(4*1024)
-#define BLOCK_SIZE			(4*1024)
+#define PAGE_SIZE            (4*1024)
+#define BLOCK_SIZE            (4*1024)
 
 int mem_fd;
 void* gpio_map;
@@ -27,31 +27,31 @@ volatile unsigned *gpio;
 #define OUT_GPIO(g) *(gpio*((g)/10)) |= (1<<(((g)%10)*3))
 #define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?>(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
-#define GPIO_SET *(gpio+7)		// sets bits that are 1
-#define GPIO_CLR *(gpio+10)		// clears bits that are 1
+#define GPIO_SET *(gpio+7)        // sets bits that are 1
+#define GPIO_CLR *(gpio+10)        // clears bits that are 1
 
-#define GET_GPIO(g) (*(gpio+13)&(1<<g))	// 0 if low 1<<g if high
+#define GET_GPIO(g) (*(gpio+13)&(1<<g))    // 0 if low 1<<g if high
 
-#define GPIO_PULL *(gpio+37)		// pull up/ pull down
-#define GPIO_PULLCLK *(gpio+38)		// pull up/pull down clock
+#define GPIO_PULL *(gpio+37)        // pull up/ pull down
+#define GPIO_PULLCLK *(gpio+38)        // pull up/pull down clock
 
 void setupIO()
 {
-	if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
-	{
-		fprintf(stderr, "failed to open /dev/mem\n");
-		exit(EXIT_FAILURE);
-	}
-	gpio_map = mmap(NULL,BLOCK_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,mem_fd,GPIO_BASE);
-	close(mem_fd);
+    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
+    {
+        fprintf(stderr, "failed to open /dev/mem\n");
+        exit(EXIT_FAILURE);
+    }
+    gpio_map = mmap(NULL,BLOCK_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,mem_fd,GPIO_BASE);
+    close(mem_fd);
 
-	if (gpio_map == MAP_FAILED)
-	{
-		fprintf(stderr, "error mapping memory%d\n", (int)gpio_map);
-		exit(EXIT_FAILURE);
-	}
+    if (gpio_map == MAP_FAILED)
+    {
+        fprintf(stderr, "error mapping memory%d\n", (int)gpio_map);
+        exit(EXIT_FAILURE);
+    }
 
-	gpio = (volatile unsigned*)gpio_map;
+    gpio = (volatile unsigned*)gpio_map;
 }
 
 /*
@@ -70,7 +70,7 @@ void lobbySel(int right){
  */
 bool voiceCMD()
 {
-	return 0;
+    return 0;
 }
 
 /*
--- src/Button_ISR.h
+++ src/Button_ISR.h
@@ -1,25 +1,83 @@
 /*
- * Author: MSOE Team Xi Senior Deisgn 2017
+ * Author: MSOE Team Xi Senior Design 2017
  * Date: 12/2017
  * Description:
  */
 
-#ifndef BUTTON_ISR_H
-#define BUTTON_ISR_H
+#include "Button_ISR.h"
 
-/*******************************************
- * External Includes                       *
- *******************************************/
-#include "AD_HOC.h"
-#include "VoiceCommands.h"
-#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-/*******************************************
- * Funtion Prototypes                      *
- *******************************************/
-void setupIO();
-void lobbySel(int);
-bool voiceCMD();
-void volModButton(int);
+#define BCM2708_PERI_BASE        0X20000000
+#define GPIO_BASE            (BCM2708_PERI_BASE + 0X00200000)
 
-#endif /* BUTTON_ISR_H */
+#define PAGE_SIZE            (4*1024)
+#define BLOCK_SIZE            (4*1024)
+
+int mem_fd;
+void* gpio_map;
+
+volatile unsigned *gpio;
+
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio*((g)/10)) |= (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?>(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)        // sets bits that are 1
+#define GPIO_CLR *(gpio+10)        // clears bits that are 1
+
+#define GET_GPIO(g) (*(gpio+13)&(1<<g))    // 0 if low 1<<g if high
+
+#define GPIO_PULL *(gpio+37)        // pull up/ pull down
+#define GPIO_PULLCLK *(gpio+38)        // pull up/pull down clock
+
+void setupIO()
+{
+    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0)
+    {
+        fprintf(stderr, "failed to open /dev/mem\n");
+        exit(EXIT_FAILURE);
+    }
+    gpio_map = mmap(NULL,BLOCK_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,mem_fd,GPIO_BASE);
+    close(mem_fd);
+
+    if (gpio_map == MAP_FAILED)
+    {
+        fprintf(stderr, "error mapping memory%d\n", (int)gpio_map);
+        exit(EXIT_FAILURE);
+    }
+
+    gpio = (volatile unsigned*)gpio_map;
+}
+
+/*
+ * Description:
+ * @param:
+ * @return:
+ */
+void lobbySel(int right){
+
+}
+
+/*
+ * Description:
+ * @param:
+ * @return:
+ */
+bool voiceCMD()
+{
+    return 0;
+}
+
+/*
+ * Description:
+ * @param:
+ * @return:
+ */
+void volModButt(int up){
+
+}
